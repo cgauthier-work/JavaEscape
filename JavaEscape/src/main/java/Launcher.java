ac4603eecdb13a5b2e94375b93d550c07a4d6e09
gauthier.candelier <gauthier.candelier@epitech.eu>
2026-04-16 10:22:34 +0200

    fixing somme error And Warn

---
+++ JavaEscape/src/main/java/Launcher.java
@@ -0,0 +1,8 @@
+import SceneHandler.StarApplication;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(StarApplication.class, args);
+    }
+}
---
+++ JavaEscape/src/main/java/SceneHandler/HelloController.java
@@ -0,0 +1,18 @@
+package SceneHandler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onPlayButtonCLick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void onQuitButtonClick(){
+        System.exit(0);
+    }
+}
